180043f35e1e670122eec10090b2e7e9c77c1221
KevinLin1120 <80439584+KevinLin1120@users.noreply.github.com>
2024-12-21 22:45:17 +0800

    Update fetching hw error

--- uiux_course/routes/course.js
+++ uiux_course/routes/course.js
@@ -615,7 +615,7 @@ router.post('/fetchHomework', isAuth, isTeacher, async function (req, res, next)
         // Prepare submissions for students with no submissions
         const newSubmissions = studentsWithNoSubmissions.map(student => ({
             isHandIn: false,
-            studentId: student.studentID,
+            studentId: student.studentID || `autoGenId-${Date.now()/100000}`,
             studentName: student.name,
             handInData: {
                 links: [],
